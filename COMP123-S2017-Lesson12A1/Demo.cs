75af2f74261cc4555c611217304ccff915d63a27
Jupiter1997 <jupiter.jv@gmail.com>
2017-08-01 15:29:24 -0400

    Created Demo

--- COMP123-S2017-Lesson12A1/Demo.cs
+++ COMP123-S2017-Lesson12A1/Demo.cs
@@ -7,12 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*
+ * Name: Marvin Jupiter Vargas
+ * Date: August 1, 2017
+ * Description: This is a Demo application to showcase user interface controls
+ * Version 0.1: Created Project
+ */
 namespace COMP123_S2017_Lesson12A1
 {
-    public partial class Form1 : Form
+    public partial class Demo : Form
     {
-        public Form1()
+        public Demo()
         {
             InitializeComponent();
         }
--- COMP123-S2017-Lesson12A1/Program.cs
+++ COMP123-S2017-Lesson12A1/Program.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*
+ * Name: Marvin Jupiter Vargas
+ * Date: August 1, 2017
+ * Description: This is a Demo application to showcase user interface controls
+ * Version 0.1: Project Creation
+ */
 namespace COMP123_S2017_Lesson12A1
 {
     static class Program
@@ -11,12 +16,12 @@ namespace COMP123_S2017_Lesson12A1
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        [STAThread] //annotation  -- leave it alone
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Demo());
         }
     }
 }
